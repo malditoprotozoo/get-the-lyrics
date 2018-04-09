1a31b8a7399a38bfa0f1084fab7f718d771a9a86
Tori Rodríguez <virodriguez.fe@gmail.com>
2018-04-09 14:28:59 -0300

    adding app

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const fetch = require('node-fetch');
+const app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+  res.render('pages/index', {lyrics: null, error: null});
+});
+
+app.post('/', function (req, res) {
+  let song = req.body.song;
+  let artist = req.body.artist;
+  let url = `https://api.lyrics.ovh/v1/${artist}/${song}`;
+  fetch(url)
+    .then(data => data.json())
+    .then(data => {
+      if (data.lyrics !== undefined) {
+        res.render('pages/index', {lyrics: data.lyrics.replace(/\n/g, '<br />'), artist: artist, song: song, error: null});
+      } else {
+        res.render('pages/index', {lyrics: null, error: data.error});
+      }
+    })
+    .catch(error => {
+      console.log('error > ' + error);
+      res.render('pages/index', {lyrics: null, error: 'Error, please try again'});
+    })
+});
+
+app.listen(3010, function () {
+  console.log('Example app listening on port 3010!')
+});
